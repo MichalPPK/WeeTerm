78e488df7e613b3b5e54640c43732f905da87e7c
MichalPPK <126679937+MichalPPK@users.noreply.github.com>
2023-03-05 14:31:28 -0500

    Update figlet.ts

--- src/utils/bin/figlet.ts
+++ src/utils/bin/figlet.ts
@@ -2,13 +2,13 @@ import * as fig from 'figlet';
 import standard from 'figlet/importable-fonts/Standard.js'
 
 export const figlet = async (args?: string[]): Promise<string> => {
-  output = args.join('+');
+  let output = args.join(' ');
 
   if (!output) {
     return 'Usage: figlet [phrase]. Example: figlet Hello World';
   }
   else {
-    return fig.text(output, {
+    return fig.text('test', {
     font: 'Standard',
 }, function(err, data) {
     return(data);

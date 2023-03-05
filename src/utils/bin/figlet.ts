aef8c9fee03f028a5622a3ce01115e5ce008bd47
MichalPPK <126679937+MichalPPK@users.noreply.github.com>
2023-03-05 08:57:20 -0500

    Update figlet.ts

--- src/utils/bin/figlet.ts
+++ src/utils/bin/figlet.ts
@@ -4,7 +4,14 @@ export const figlet = async (args?: string[]): Promise<string> => {
   let output = '';
 
   if (args.length < 1 || args[0] === '') {
-    return fig.text('Error');
+    return fig.text('Error'', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
   } else {
     output = args.join(' ');
     return fig(output);

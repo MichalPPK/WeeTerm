3c21f7154ca136ed844b06953bb2e60a71590f14
MichalPPK <126679937+MichalPPK@users.noreply.github.com>
2023-03-04 19:32:56 -0500

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -22,6 +22,11 @@ export const getWeather = async (city: string) => {
   }
 };
 
+export const getFortune = async () => {
+  const { data } = await axios.get('https://helloacm.com/api/fortune/');
+  return data;
+};
+
 export const getQuote = async () => {
   const { data } = await axios.get('https://api.quotable.io/random');
   return {

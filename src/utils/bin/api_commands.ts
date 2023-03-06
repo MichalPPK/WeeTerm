f19f2c9212ce6c000cd705cb7c64368fd32232a5
MichalPPK <126679937+MichalPPK@users.noreply.github.com>
2023-03-06 08:52:25 -0500

    Update api_commands.ts

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -54,7 +54,7 @@ export const number = async (args: string[]): Promise<string> => {
 };
 
 export const datehis = async (args: string[]): Promise<string> => {
-  let datehist = '';
+  let datehist = args;
   if (!datehist) {
     return 'Get random trivia about a date.\n Usage: datehis [MONTH/DAY]. Example: datehis 8/22';
   }

14eb7297c15ea8662a2ad941db28b08d67ea9253
MichalPPK <126679937+MichalPPK@users.noreply.github.com>
2023-03-06 19:40:17 -0500

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -38,7 +38,7 @@ export const getFiglet = async (phrase: string) => {
 
 export const getNumber = async (number: string) => {
   try {
-    const { data } = await axios.get(`http://numbersapi.com/${number}`,{insecureHTTPParser: true});
+    const { data } = await axios.get(`http://numbersapi.com/${number}`,{transport: http});
     return data;
   } catch (error) {
     return error;

78f0caddbc3c370febf6ccd64b1b92b00bae32d4
MichalPPK <126679937+MichalPPK@users.noreply.github.com>
2023-03-06 20:09:12 -0500

    Update figlet.ts

--- src/utils/bin/figlet.ts
+++ src/utils/bin/figlet.ts
@@ -5,7 +5,7 @@ export const figgy = async (args?: string[]): Promise<string> => {
   let output = args.join(' ');
 
   if (!output) {
-    return 'Usage: figlet [phrase]. Example: figlet Hello World';
+    return 'Usage: figgy [phrase]. Example: figgy Hello World';
   }
   else {
     return fig.text(output, {
